bd60302fdacf5bce022bdcfa776f3dd0a21ab2fb
Robert Parasca <robert.parasca@grapefruit.ro>
2022-04-17 19:24:38 +0300

    chore: initial commit setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    experimental: {
+        // Enables the styled-components SWC transform
+        styledComponents: true,
+    },
+    images: {
+        domains: ['via.placeholder.com'],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
